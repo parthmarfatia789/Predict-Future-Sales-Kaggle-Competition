af506bb126b4ea260b1284f1ddf846e370cbe88a
parthmarfatia789 <66524229+parthmarfatia789@users.noreply.github.com>
2020-07-25 16:20:49 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 25 16:19:33 2020
+
+@author: Mahe
+"""
+
+
---
+++ main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 25 16:18:27 2020
+
+@author: Mahe
+"""
+
+
+######### Kaggle Competition ##########
